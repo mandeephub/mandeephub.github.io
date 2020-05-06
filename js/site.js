bbb243a7de7ebb58ced93d4e2c0f82dabe236f74
mandeephub <suggestions.live@gmail.com>
2020-05-06 14:17:03 +0530

    new code

--- js/site.js
+++ js/site.js
@@ -23,3 +23,18 @@ $(document).ready(function(){
   });
 });
 
+
+
+// blog sidebar js 
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("open-btn").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("open-btn").style.display = "block";
+}
